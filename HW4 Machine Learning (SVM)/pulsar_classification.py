7678e192cc759d34c11630d69a1a0a40d12419df
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-12-02 14:31:02 -0800

    Data Input Console Messages finished
    Learned a lot of pandas lib stuff

--- HW4 Machine Learning (SVM)/pulsar_classification.py
+++ HW4 Machine Learning (SVM)/pulsar_classification.py
@@ -1,33 +1,31 @@
 # Import libraries
-
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_classification
 
 import pickle
-import csv
 import pandas as pd
+import numpy as np
+
 # In this section, you can use a search engine to look for the functions that will help you implement the following steps
 
 # Load dataset and show basic statistics
-try:
-    with open('pulsar_stars.csv', newline='') as csvFile:
-        pulsarData = list(csv.reader(csvFile))
-    print(pulsarData)
-except OSError as error:
-    print('File failed to open!')
-    raise 
+pulsarData = pd.read_csv('pulsar_stars.csv', sep=', |,', dtype=np.float64, engine="python")
 # 1. Show dataset size (dimensions)
-
+print('Data Size: ' + str(len(pulsarData)))
 # 2. Show what column names exist for the 9 attributes in the dataset
+print('\nAttribute Names:')
+print(pulsarData.dtypes)
 # 3. Show the distribution of target_class column
 # 4. Show the percentage distribution of target_class column
-
-
-
-
-
+neg_class, pos_class = 0, 0
+for entry in pulsarData['target_class']:
+    if entry == 0:
+        neg_class += 1
+    else:
+        pos_class += 1
+print(f'\nDistribution:\n Negative: {neg_class} ({(neg_class/len(pulsarData))*100}%)\n Positive: {pos_class} ({(pos_class/len(pulsarData))*100}%)\n')
 # Separate predictor variables from the target variable (X and y as we did in the class)
 
 
